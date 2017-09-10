d0c9e9474aa6dffdfb78291128c20375c727f7e4
ThiMB <thimb2012@gmail.com>
2017-09-11 02:14:44 +0700

    update

--- db.sql
+++ db.sql
@@ -1,14 +1,155 @@
-CREATE TABLE `m_user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'surrogate key',
-  `email_address` tinytext COMMENT 'メールアドレス＝ログイン名',
-  `name` tinytext COMMENT 'ユーザーの名前',
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo
+-- ------------------------------------------------------
+-- Server version	5.6.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='ユーザー情報の管理';
-SELECT * FROM demo.m_user;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `contact`
+--
 
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (1,'a','a','a');
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `m_role`
+--
+
+DROP TABLE IF EXISTS `m_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `m_role` (
   `id` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `m_role`
+--
+
+LOCK TABLES `m_role` WRITE;
+/*!40000 ALTER TABLE `m_role` DISABLE KEYS */;
+INSERT INTO `m_role` VALUES (1,'Admin'),(2,'Editor'),(3,'Reader');
+/*!40000 ALTER TABLE `m_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `m_user`
+--
+
+DROP TABLE IF EXISTS `m_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `m_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'surrogate key',
+  `email_address` tinytext COMMENT 'メールアドレス＝ログイン名',
+  `name` tinytext COMMENT 'ユーザーの名前',
+  `role_id` int(11) DEFAULT NULL,
+  `password` tinytext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COMMENT='ユーザー情報の管理';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `m_user`
+--
+
+LOCK TABLES `m_user` WRITE;
+/*!40000 ALTER TABLE `m_user` DISABLE KEYS */;
+INSERT INTO `m_user` VALUES (1,'admin','admin name',1,'1'),(2,'reader','reader name',3,'1'),(23,'a@gmail.com','a',1,'$2a$11$90X9v4osfsI5wW.DaDBB4.0lXwlVBMvB27mUQL..siSfl0OdX1HAq'),(24,'b@gmail.com','b',3,'$2a$11$vgOUv.AKhDcjlPhZ2yNoJeJOh3U7gbenAyDNd6PiTKVMOyiT2F6BG'),(25,'c@gmail.com','c',1,'$2a$11$734B8MuXKg96SNlO0ii0s.uJLV/UDerjNlAW6FJ.tpj9kQdkZq2PS'),(26,'1@gmail.com','s',1,'$2a$11$b9LpqNGu9dEBgzVYpq7ZSOCE/QHmAkFwawxTvQSvSUzKEsk69tvvC');
+/*!40000 ALTER TABLE `m_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `privilege`
+--
+
+DROP TABLE IF EXISTS `privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `privilege` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `privilege`
+--
+
+LOCK TABLES `privilege` WRITE;
+/*!40000 ALTER TABLE `privilege` DISABLE KEYS */;
+INSERT INTO `privilege` VALUES (1,'USER_READ'),(2,'USER_WRITE'),(3,'USER_LIST'),(4,'USER_MANAGE');
+/*!40000 ALTER TABLE `privilege` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles_privileges`
+--
+
+DROP TABLE IF EXISTS `roles_privileges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles_privileges` (
+  `role_id` int(11) NOT NULL,
+  `privilege_id` bigint(20) NOT NULL,
+  KEY `FKp0x1d9k5aksyqd1akwwfkh0ki` (`privilege_id`),
+  KEY `FK2rfl694fu6ls2f2mqcxesqc6p` (`role_id`),
+  CONSTRAINT `FK2rfl694fu6ls2f2mqcxesqc6p` FOREIGN KEY (`role_id`) REFERENCES `m_role` (`id`),
+  CONSTRAINT `FKp0x1d9k5aksyqd1akwwfkh0ki` FOREIGN KEY (`privilege_id`) REFERENCES `privilege` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles_privileges`
+--
+
+LOCK TABLES `roles_privileges` WRITE;
+/*!40000 ALTER TABLE `roles_privileges` DISABLE KEYS */;
+INSERT INTO `roles_privileges` VALUES (1,1),(1,2),(3,1),(1,3),(3,3),(1,4);
+/*!40000 ALTER TABLE `roles_privileges` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-08 10:12:59
